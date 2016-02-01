fc929d429e9114a1c4ceecaae0e4054fdd073839
Amit Bhagat <scet.amit@gmail.com>
2016-01-31 23:05:20 -0800

    Initial commit

---
+++ openbmp_streaming.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+from pymongo import MongoClient
+import netaddr
+from contextlib import contextmanager
+
+
+@contextmanager
+def connect_to_db():
+    conn = MongoClient('mongodb://localhost',port=27017)
+    db = conn.openbmp
+    yield db
+    conn.close()
+
+def check_for_hijack(record_entry):
+    our_ranges = ['1.0.0.0/8','172.16.0.0/16']
+    our_asn = '300'
+    if record_entry['Action'] == 'add': 
+        prefix = record_entry['Prefix']
+        prefixlen = record_entry['PrefixLen']
+        origin_asn = record_entry['Origin_AS']
+        as_path = record_entry['AS_Path'].rstrip().split()
+        match_found = netaddr.all_matching_cidrs(prefix,our_ranges)
+        if match_found and our_asn != origin_asn and our_asn not in as_path:
+            prefix = prefix + '/' + prefixlen
+            message = 'Router {} - Prefix {} appers to be hijacked or incorrectly leaked.'.format(record_entry['Router_IP'],prefix) 
+            with connect_to_db() as db:
+                db.alerts.insert({'timestamp':record_entry['Timestamp'],'message':message,'originating_asn':origin_asn,'as_path':record_entry['AS_Path'],'mitigated':True}) 
+            
+def sendPartition(iter):
+    for record in iter:
+        check_for_hijack(record)
+        with connect_to_db() as db:
+            db.unicast_prefix.insert(record)
+
+def structure_data(line):
+    fields = ['Action',
+    'Sequence',
+    'Hash',
+    'Router_Hash',
+    'Router_IP',
+    'Base_Attr_Hash',
+    'Peer_Hash',
+    'Peer_IP',
+    'Peer_ASN',
+    'Timestamp',
+    'Prefix',
+    'PrefixLen',
+    'isIPv4',
+    'Origin',
+    'AS_Path',
+    'AS_Path_Count',
+    'Origin_AS',
+    'Next_Hop',
+    'MED',
+    'Local_Pref',
+    'Aggregator',
+    'Community_List',
+    'Ext_Community_List',
+    'Cluster_List',
+    'isAtomicAgg',
+    'isNextHopIPv4',
+    'Originator_Id']
+
+    d = dict()
+    for k,v in zip(fields,line.split('\t')):
+        d[k] = str(v)
+    return d
+
+
+sc = SparkContext(appName="OpenBMPViaKafka")
+ssc = StreamingContext(sc,5)
+
+zkQuorum = 'localhost:2181'
+topic = 'openbmp.parsed.unicast_prefix'
+partition = 1
+kafkaStream = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: partition})
+#kafkaStream.pprint()
+
+lines = kafkaStream.flatMap(lambda x: x[1].splitlines()).filter(lambda line: line.startswith('add') or line.startswith('del'))
+
+result = lines.map(structure_data)
+#result.pprint()
+
+result.foreachRDD(lambda rdd: rdd.foreachPartition(sendPartition))
+
+ssc.start()
+ssc.awaitTermination()
+
